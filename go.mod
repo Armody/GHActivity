b067927214121482ce3328c083b4ded40c511624
Armody <Rmdyzie@gmail.com>
2025-05-22 17:57:25 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Armody/GHActivity
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("Expecting a username")
+		return
+	}
+}
